53e0454ad879e58efe626c4ef64af1ed36ed7973
AbderazakAmiar <abderazak Amiar>
2020-01-16 00:09:41 +0100

    init

---
+++ app/src/main/java/dz/apps/android_projects_basic_structure/Activities/MainActivity.java
@@ -0,0 +1,20 @@
+package dz.apps.android_projects_basic_structure.Activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import dz.apps.android_projects_basic_structure.R;
+
+public class MainActivity extends AppCompatActivity {
+
+@Override
+protected void onCreate(Bundle savedInstanceState) {
+
+	super.onCreate(savedInstanceState);
+	setContentView(R.layout.activity_main);
+
+	//This project shows you the basic structure of commonly used packages.
+	////////////////////////////////////////////////////////////////////////
+
+    }
+}
