16b044129e1843d4dc0c97ec0c9ac8e2fe6c2bfb
AbderazakAmiar <abderazak Amiar>
2020-01-16 00:22:46 +0100

    commit_16-01-2020

---
+++ app/src/main/java/dz/apps/android_projects_basic_structure/Adapters/Adapters.java
@@ -0,0 +1,4 @@
+package dz.apps.android_projects_basic_structure.Adapters;
+
+public class Adapters {
+}
---
+++ app/src/main/java/dz/apps/android_projects_basic_structure/Utils/Utils.java
@@ -0,0 +1,4 @@
+package dz.apps.android_projects_basic_structure.Utils;
+
+public class Utils {
+}
